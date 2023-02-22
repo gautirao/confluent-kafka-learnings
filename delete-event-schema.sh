ac28d98b1431f7a3d841ab13ab135b9c9b9dc695
Goutham Rao <goutham.k.rao@gmail.com>
2023-02-22 10:24:20 +0000

    simplified schema name spaces

--- delete-event-schema.sh
+++ delete-event-schema.sh
@@ -2,9 +2,9 @@
 curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/creditevents-value
 curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/creditevents-value?permanent=true
 
-curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/io.confluent.union.example.avro.event.EventPayload_v1_0
-curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/io.confluent.union.example.avro.event.EventEnvelope_v1_0
-curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/io.confluent.union.example.avro.event.EventEnvelope_v1_1
-curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/io.confluent.union.example.avro.event.EventEnvelope_v1_0?permanent=true
-curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/io.confluent.union.example.avro.event.EventEnvelope_v1_1?permanent=true
-curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/io.confluent.union.example.avro.event.EventPayload_v1_0?permanent=true
+curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/com.natwest.event.v1_0.EventPayload
+curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/com.natwest.event.v1_0.EventEnvelope
+curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/com.natwest.event.v1_1.EventEnvelope
+curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/com.natwest.event.v1_0.EventEnvelope?permanent=true
+curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/com.natwest.event.v1_1.EventEnvelope?permanent=true
+curl -X DELETE -u 6VRQFCHS5PPL5AIG:jT2fieiR819YKb0VbTAH1fgX/urzkBT31xMa19VBOZMlivvjpgrjpHtRbDjf9DZb https://psrc-8kz20.us-east-2.aws.confluent.cloud/subjects/com.natwest.event.v1_0.EventPayload?permanent=true
--- src/main/java/io/confluent/examples/clients/basicavro/EventProducer.java
+++ src/main/java/io/confluent/examples/clients/basicavro/EventProducer.java
@@ -1,6 +1,8 @@
 package io.confluent.examples.clients.basicavro;
 
-import io.confluent.union.example.avro.event.*;
+import com.natwest.event.CreditEventV1_0;
+import com.natwest.event.v1_0.EventEnvelope;
+import com.natwest.event.v1_0.PayloadValue;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
@@ -30,22 +32,21 @@ public class EventProducer {
             e.printStackTrace();
         }
     }
-
     private static CreditEventV1_0 createCreditEvent(int count) {
 
         return CreditEventV1_0.newBuilder().setEventUniqueId(count+"").setSchemaId("CreditEvent_V1_0").setEnvelope(getEnvelope(count)).setPayload(getPayload(count)).build();
     }
 
-    private static EventPayload_v1_0 getPayload(int count) {
-        return EventPayload_v1_0.newBuilder().setSchemaId("paylaod_"+count).setValue(getPayloadValue(count)).build();
+    private static com.natwest.event.v1_0.EventPayload getPayload(int count) {
+        return com.natwest.event.v1_0.EventPayload.newBuilder().setSchemaId("paylaod_"+count).setValue(getPayloadValue(count)).build();
     }
 
-    private static PayloadValue getPayloadValue(int count) {
+    private static com.natwest.event.v1_0.PayloadValue getPayloadValue(int count) {
         return PayloadValue.newBuilder().setCreditAmount(count).setJourney("marketing").build();
     }
 
-    private static EventEnvelope_v1_0 getEnvelope(int count) {
-        return EventEnvelope_v1_0.newBuilder().setEventType(EventType.APPROVAL_EVENT).setSchemaId("envelope_"+count).setLineOfBusiness(lineOfBusiness.MKTG).build();
+    private static EventEnvelope getEnvelope(int count) {
+        return com.natwest.event.v1_0.EventEnvelope.newBuilder().setEventType(com.natwest.event.v1_0.EventType.APPROVAL_EVENT).setSchemaId("envelope_"+count).setLineOfBusiness(com.natwest.event.v1_0.lineOfBusiness.MKTG).build();
 
     }
 }
